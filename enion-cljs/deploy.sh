43ea629f28d6da61905b1ede33dc6485e63b6ec0
ertugrulcetin <ertu.ctn@gmail.com>
2023-02-20 22:12:38 +0300

    fix sentry

--- enion-cljs/deploy.sh
+++ enion-cljs/deploy.sh
@@ -2,7 +2,7 @@ export ENION_SHA=$(git rev-parse HEAD)
 npm run clean
 npm run release
 sentry-cli releases --org enion-online --project enion-online new ${ENION_SHA}
-sentry-cli releases --org enion-online --project enion-online files ${ENION_SHA} upload-sourcemaps resources/public/js/compiled --validate --url-prefix "~/compiled"
+sentry-cli releases --org enion-online --project enion-online files ${ENION_SHA} upload-sourcemaps resources/public/js/compiled
 sentry-cli releases --org enion-online --project enion-online finalize ${ENION_SHA}
 rm resources/public/js/compiled/app.js.map
 rm -rf resources/public/js/compiled/cljs-runtime
