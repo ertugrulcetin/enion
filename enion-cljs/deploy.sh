ec3d2d0d7385ac800e30fcfb7b93641761e86849
ertugrulcetin <ertu.ctn@gmail.com>
2023-03-12 20:14:14 +0300

    update sh

--- enion-cljs/deploy.sh
+++ enion-cljs/deploy.sh
@@ -12,4 +12,11 @@ cp -r resources ../enion-backend
 cd ../enion-backend
 lein clean
 lein uberjar
-fly deploy --local-only
+
+fly deploy --local-only --image-label ${ENION_SHA}
+
+fly deploy -a enion-eu-1 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-eu-2 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-eu-3 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-br-1 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-br-2 --local-only --image-label ${ENION_SHA} &
