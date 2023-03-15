f46f23647fd3f55e2bda2ce8cbee97050b7c8187
ertugrulcetin <ertu.ctn@gmail.com>
2023-03-15 14:08:29 +0300

    added sh dir

---
+++ enion-cljs/sh/backend-deploy.sh
@@ -0,0 +1,12 @@
+export ENION_SHA=$(git rev-parse HEAD)
+cd ../enion-backend
+lein clean
+lein uberjar
+
+fly deploy --local-only --image-label ${ENION_SHA}
+
+fly deploy -a enion-eu-1 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-eu-2 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-eu-3 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-br-1 --local-only --image-label ${ENION_SHA} & \
+fly deploy -a enion-br-2 --local-only --image-label ${ENION_SHA} &
